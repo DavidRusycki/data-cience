6f04f9ddf5b08a8f2b0dc94fc8f5587e59c0c9fe
David Rusycki <davidrusyck@gmail.com>
2023-02-28 00:19:39 +0000

    aula 3

---
+++ unidade1.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+@st.cache_data()
+def load_database():
+    return pd.read_csv('./arquivos/brasil_estados.csv')
+
+st.title('Meu primeiro App - GCI')
+estados = load_database()
+st.dataframe(estados)
+
+dados, estatistica = st.tabs(
+
+    ['Dados', 'Estatística Descritiva']
+
+)
+
+with dados:
+    regiao = st.selectbox(
+        'Selecione a região:',
+        estados['regiao_nome'].unique()
+    )
+    st.dataframe(estados[estados['regiao_nome'] == regiao])
+
+with estatistica:
+    variavel = st.selectbox(
+        'Selecione a variavel',
+        ['area', 'populacao', 'idh', 'matricula']
+    )
+    st.table(estados[variavel].describe())
